db08d76d0db0d14ce7839726abc3e549b25b9c42
Gideon Jacob <gideonjacob.in@gmail.com>
2025-01-30 13:49:40 +0000

    Added Profile Card

---
+++ src/components/ProfileCard/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import './index.sass'
+
+const ProfileCard: React.FC = () => {
+    return (
+        <div className="profile-card">
+            <img
+                src="https://media.licdn.com/dms/image/v2/D4D03AQG2KWjigyc7QA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1718249729153?e=1743638400&v=beta&t=zgsnCkSh6zAa77aQ4hAVb9uGTz8gdms05-xkUzw04XA" 
+                alt="avatar"
+                loading="lazy"
+            />
+
+            <div>
+                <h1>Gideon Jacob</h1>
+                <p>Full Stack Developer | Docker | AWS | Sass | React</p>
+            </div>
+
+            <a
+                href="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" 
+                target="_blank"
+                className="btn btn-outline btn-sm text-xs mt-6 opacity-50"
+                download
+                rel="noreferrer"
+            >
+                Download Resume
+            </a>
+        </div>
+    );
+}
+
+export default ProfileCard
