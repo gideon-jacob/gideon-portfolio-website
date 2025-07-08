61ce3e31dd5148d6c7b85341bbf08349143b5d19
Gideon Jacob <gideonjacob.in@gmail.com>
2025-07-08 22:03:55 +0530

    refactor(project-details): updated tumbnail images and few project description.

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -13,6 +13,7 @@ interface Project {
   title: string;
   thumbnail: string;
   description: string;
+  warningInfo?: string;
   githubLinks: {
     client?: string;
     server?: string;
@@ -35,7 +36,7 @@ const projects: Project[] = [
   {
     id: uuidv4(),
     title: 'PEC Student Hub',
-    thumbnail: 'https://images.indianexpress.com/2023/11/anthropic-claude-featured.jpg',
+    thumbnail: 'https://res.cloudinary.com/ddixkodsj/image/upload/t_Banner 16:9/v1751958535/pec-student-1_jwxji3.png',
     description: 'PEC Student Hub is a student-centric application designed to provide a seamless and user-friendly interface for managing academic information.',
     githubLinks: {
       client: 'https://github.com/gideon-jacob/pec-student-client',
@@ -49,7 +50,7 @@ const projects: Project[] = [
   {
     id: uuidv4(),
     title: 'Gideon Portfolio Website',
-    thumbnail: 'https://images.indianexpress.com/2023/11/anthropic-claude-featured.jpg',
+    thumbnail: 'https://res.cloudinary.com/ddixkodsj/image/upload/t_Banner%2016:9/v1751984298/portfolio_y1du0q.png',
     description: 'This is a portfolio website for Gideon Jacob, a software engineer.',
     githubLinks: { src: 'https://github.com/gideon-jacob/gideon-portfolio-website'},
     liveLink: 'https://portfolio.gideonjacob.in/',
@@ -59,19 +60,20 @@ const projects: Project[] = [
   },
   { 
     id: uuidv4(),
-    title: 'Aquatic Drone Web Interface',
-    thumbnail: 'https://images.indianexpress.com/2023/11/anthropic-claude-featured.jpg',
-    description: 'This is a prototype of an aquatic drone interface.',
+    title: 'Marine Drone Mission Planner',
+    thumbnail: 'https://res.cloudinary.com/ddixkodsj/image/upload/t_Banner%2016:9/v1751988031/aquatic-drone-web_ndfcew.png',
+    description: 'A proof-of-concept web interface for planning and executing aquatic drone missions. It ensures operational safety and efficiency with intuitive geofencing, real-time path tracking, and a fail-safe "Return to Base" function.',
+    warningInfo: 'This live demo is a simulation and is not connected to a real drone.',
     githubLinks: { src: 'https://github.com/gideon-jacob/aquaticDrone'},
     liveLink: 'https://aquaticdrone.gideonjacob.in/',
-    technologies: ['React', 'TypeScript', 'Vite', 'Sass', 'Docker'],
+    technologies: ['React', 'TypeScript', 'Vite', 'Sass', 'Docker', 'Google Maps API'],
     devices: [device.COMPUTER],
     isCompleted: true,
   },
   {
     id: uuidv4(),
-    title: 'NxtTalk Web App Clone',
-    thumbnail: 'https://miro.medium.com/v2/resize:fit:1200/0*UXRL6AmR6mpIMen_.jpg',
+    title: 'NxtTalk Clone',
+    thumbnail: 'https://res.cloudinary.com/ddixkodsj/image/upload/t_Banner%2016:9/v1751985163/nxt-talks_blpprr.png',
     description: 'This is a web clone of the NxtTalk app. (https://portal.ccbp.in/nxt-talks)',
     githubLinks: { src: 'https://github.com/gideon-jacob/nxt-talk-web-clone'},
     technologies: ['HTML', 'CSS', 'Sass'],
@@ -81,7 +83,7 @@ const projects: Project[] = [
   {
     id: uuidv4(),
     title: 'Youtube Clone',
-    thumbnail: 'https://miro.medium.com/v2/resize:fit:1200/0*UXRL6AmR6mpIMen_.jpg',
+    thumbnail: 'https://res.cloudinary.com/ddixkodsj/image/upload/t_Banner%2016:9/v1751985964/youtube_wxteqq.png',
     description: 'This is a web clone of the Youtube app. (https://www.youtube.com/)',
     githubLinks: { src: 'https://github.com/gideon-jacob/youtube-clone'},
     technologies: ['HTML', 'Sass', 'Node.js'],
@@ -91,7 +93,7 @@ const projects: Project[] = [
   {
     id: uuidv4(),
     title: 'Netflix Clone',
-    thumbnail: 'https://miro.medium.com/v2/resize:fit:1200/0*UXRL6AmR6mpIMen_.jpg',
+    thumbnail: 'https://res.cloudinary.com/ddixkodsj/image/upload/t_Banner%2016:9/v1751986411/netflix_wye7ds.png',
     description: 'This is a web clone of the Netflix app. (https://www.netflix.com/)',
     githubLinks: { src: 'https://github.com/gideon-jacob/netflix-clone'},
     technologies: ['HTML', 'CSS', 'Sass', 'Node.js'],
@@ -129,6 +131,7 @@ const Home: React.FC = () => {
                 technologies={project.technologies}
                 devices={project.devices}
                 isCompleted={project.isCompleted}
+                warningInfo={project.warningInfo}
               />
             ))}
           </div>
